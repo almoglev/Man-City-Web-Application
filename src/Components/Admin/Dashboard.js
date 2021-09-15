c8dc2413065b62967acdc5fd258f0fa823acd8ee
Almog Lev <almoglev3@gmail.com>
2021-09-15 18:24:30 +0300

    adding auth guard

---
+++ src/Components/Admin/Dashboard.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Dashboard = (props) => {
+    return(
+        <>
+            <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+            Dashboard
+        </>
+    )
+}
+export default Dashboard;
